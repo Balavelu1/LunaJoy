0701fcec7fc88d5c4824505a17a58c19b60f5cf3
Balavelu1 <vsakthibala99@gmail.com>
2024-05-17 12:46:41 +0530

    page inspection

--- src/test/java/org/Runner/Run.java
+++ src/test/java/org/Runner/Run.java
@@ -10,12 +10,11 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\Users\\User\\eclipse-workspace2\\Lunajoy\\src\\test\\java\\Features_Luna",
-glue = "org.Steps_Luna", dryRun =false,monochrome=true
+@CucumberOptions(features = "C:\\Users\\User\\eclipse-workspace2\\Lunajoy\\src\\test\\java\\Features_Luna", 
+glue = "org.Steps_Luna", dryRun = false, monochrome = true
 
 )
-	
-	
+
 public class Run {
 	public static WebDriver driver;
 
@@ -25,13 +24,11 @@ public class Run {
 
 		driver = new ChromeDriver();
 	}
-	
+
 	@AfterClass
 	public static void closebro() {
 		driver.quit();
 
 	}
 
-
 }
-	
